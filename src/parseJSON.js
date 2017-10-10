8f9e19eb3fb733707254c30d65a304485a124659
jeffreystocker <kagesennin@gmail.com>
2017-10-09 20:11:30 -0700

    started json parse- finished arrays

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -3,5 +3,57 @@
 
 // but you're not, so you'll write it from scratch:
 var parseJSON = function(json) {
-  // your code goes here
+  var output;
+  //1st inspect character to detemine type of object/array/string
+  //find the end character for that array/object
+    //if array, put into array function
+  if (json[0] === '[') {
+    output = [];
+    for (var i = 1; i < json.length; i++) {
+      if (json[i] === ']') {
+        break;
+      } else if (json[i] === '[') {
+        output = output.concat(parseJSON(json.slice(json[i], json.length - 1)));
+      } else {
+        output.push(json[i]);
+      }
+
+    }
+  }
+  //  '{"foo": ""}',
+  if (json[0] === '{') {
+    output = {};
+    for (var i = 1; i < json.length; i++) {
+      if (json[i] === '}') {
+        break;
+      } else if (json[i] === '{') {
+        output = output.concat(parseJSON(json.slice(json[i], json.length - 1)));
+      } else {
+        var indexOfColon = json.indexOf(':', i);  //ending for before lectures
+        output.push(json[i]);
+      }
+    }
+  }
+    //if object, put into object function
+    //
+  return output;
 };
+
+var parseJSONArray = function (string) {
+  var newObject={};
+  
+  return;
+};
+
+//code i don't want to thoss yet
+      // var lookatValue = json[i];
+      // if (lookatValue === '[') {
+      //   arrayIndex++;    
+      // } else if (lookatValue === ']') {
+      //   if (arrayIndex === 1) {
+      //     // where recoursion will happen
+      //     parseJSON (json.splice(2, i-1));
+      //   } else {
+      //     arrayIndex--;
+      //   }
+    //   }
